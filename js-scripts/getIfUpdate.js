8c0597b59858ef95290c68b122ce30b1d3a16a0a
matteo-moro <moro.matteorosario@gmail.com>
2026-01-02 23:39:07 +0100

    Add files via upload

---
+++ js-scripts/getIfUpdate.js
@@ -0,0 +1,48 @@
+var request= new XMLHttpRequest();
+
+request.onreadystatechange = function(response)
+{
+	if (request.readyState === 4 && request.status === 200)
+	{
+		var data = JSON.parse(request.responseText);
+		if (!(kicakStorage.getItem("version") === data.version) || data.forceupdate === "true")
+		{
+			if (storageAvailable("localStorage"))
+			{
+				localStorage.setItem("version", data.version);
+				localStorage.setItem("reloadJSON", "true");
+			}
+		}
+		else
+		{
+			if (storageAvailable("localStorage"))
+			{
+				localStorage.setItem("reloadJSON", "false");
+			}
+		}
+	}
+}
+
+request.open("GET", "json-files/version.json", true);
+reqyest.send();
+
+//directly taken from mdn docs
+function storageAvailable(type)
+{
+	let storage;
+	try
+	{
+		storage = window[type];
+		const x = "__storage_test__";
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	} catch(e) {
+		return (
+			e instanceof DOMException &&
+			e.name === "QuotaExceededError" &&
+			storage &&
+			storage.lenght !== 0
+		);
+	}
+}
---
+++ js-scripts/parsePokemonJson.js
@@ -0,0 +1,10 @@
+var pokemonNameList = document.getElementById("pokemon-name-datalist");
+
+var data = JSON.parse(localStorage.getItem("pokemon-json"));
+
+data.pokemonlist.forEach(function(pokemon)
+{
+	var option = document.createElement("option");
+	option.value = pokemon.name;
+	pokemonNameList.appendChild(option);
+})
